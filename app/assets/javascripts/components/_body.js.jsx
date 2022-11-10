dfcc4f6c609ffc77abd38147fbc9c3ec72f57bc0
KeziahOkyere41 <nharnhayhaar208@gmail.com>
2022-11-09 13:35:03 -1200

    project-fruits

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,45 @@
+class Body extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          fruits: []
+        };
+        this.handleUpdate = this.handleUpdate.bind(this);
+    this.updateFruit = this.updateFruit.bind(this)
+  }
+
+  handleUpdate(fruit){
+    fetch(`http://localhost:3000/api/v1/fruits/${fruit.id}`, 
+    {
+      method: 'PUT',
+      body: JSON.stringify({fruit: fruit}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => { 
+        this.updateFruit(fruit)
+      })
+  }
+  updateFruit(fruit){
+    let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id)
+    newFruits.push(fruit)
+    this.setState({
+      fruits: newFruits
+    })
+  }
+
+  componentDidMount(){
+    fetch('/api/v1/items.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ items: data }) });
+  }
+
+  render(){
+    return(
+      <div>
+        <NewItem handleSubmit = {this.handleSubmit} />
+        <AllItems items={this.state.items} handleDelete = {this.handleDelete} handleUpdate = {this.handleUpdate}/>
+      </div>
+    )
+  }
+    }
